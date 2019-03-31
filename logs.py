9f44a38ba88c59280d721fd150cc689ca76816dc
Kaan Eroglu <kaan@kaan.ca>
2019-03-30 19:48:21 -0600

    Modified comments and updated readme

--- logs.py
+++ logs.py
@@ -1,78 +1,74 @@
-# "Database code" for the DB Forum.
+#!/usr/bin/env python3
+
+"""This code was created as a project submission to Udacity's Full Stack Web Developer Nanodegree program.
+It demonstrates ability to connect to a PostgreSQL database and run different SELECT queries based on 3 questions,
+then displays the results in a formatted/readable way. Please check the README file for instructions to run the code."""
 
 import psycopg2
-import sys
 
-# Database connection for PostgreSQL
-db = psycopg2.connect("dbname=news")
+# Query for "What are the most popular three articles of all time?"
+pop_artcl_qry = """SELECT articles.title, views.view_count
+              FROM articles,
+              (SELECT SUBSTRING (path, 10) AS article_title, count(id) AS view_count FROM log WHERE path like '/article/%' group by article_title) AS views
+              WHERE articles.slug LIKE views.article_title
+              ORDER BY views.view_count desc LIMIT 3"""
 
-# Queries for functions (popular article query, popular author query, day error query)
-pop_artcl_qry= "SELECT articles.title, views.view_count " \
-               "FROM articles, " \
-                "(SELECT SUBSTRING (path, 10) AS article_title, count(id) AS view_count FROM log WHERE path like '/article/%' group by article_title) AS views " \
-               "WHERE articles.slug LIKE views.article_title " \
-               "ORDER BY views.view_count desc LIMIT 3"
+# Query for "Who are the most popular article authors of all time?"
+pop_auth_qry = """SELECT authors.name, sum(views.view_count) as author_views
+              FROM articles,
+              (SELECT SUBSTRING (path, 10) AS article_title, count(id) AS view_count FROM log WHERE path like '/article/%' group by article_title) AS views,
+              authors
+              WHERE articles.slug LIKE views.article_title
+              AND authors.id = articles.author
+              GROUP BY authors.name
+              ORDER BY author_views desc"""
 
-pop_auth_qry= "SELECT authors.name, sum(views.view_count) as author_views " \
-              "FROM articles, (SELECT SUBSTRING (path, 10) AS article_title, count(id) AS view_count FROM log WHERE path like '/article/%' group by article_title) AS views, authors " \
-              "WHERE articles.slug LIKE views.article_title AND authors.id = articles.author " \
-              "GROUP BY authors.name " \
-              "ORDER BY author_views desc;"
-
-day_error_qry= "SELECT * " \
-              "FROM " \
-              "(SELECT error_table.error_days, ROUND(((error_table.error_count*100.0)/total_table.view_count),2) as error_perc " \
-                "FROM (SELECT count(id) as view_count, to_char(date_trunc('day', time), 'YYYY-MM-DD') as view_days FROM log GROUP BY view_days) total_table " \
-              "LEFT JOIN " \
-              "(SELECT count(id) as error_count, to_char(date_trunc('day', time), 'YYYY-MM-DD') as error_days " \
-                "FROM log WHERE status like '404 NOT FOUND' GROUP BY error_days) error_table " \
-              "ON (total_table.view_days = error_table.error_days)) results " \
-              "WHERE error_perc>1"
+# Query for "On which days did more than 1% of requests lead to errors?"
+day_error_qry = """SELECT *
+              FROM
+              (SELECT error_table.error_days, ROUND(((error_table.error_count*100.0)/total_table.view_count),2) as error_perc
+              FROM (SELECT count(id) as view_count, time::date as view_days FROM log GROUP BY view_days) total_table
+              LEFT JOIN
+              (SELECT count(id) as error_count, time::date as error_days FROM log WHERE status like '404 NOT FOUND' GROUP BY error_days) error_table
+              ON (total_table.view_days = error_table.error_days)) results
+              WHERE error_perc>1"""
 
 
-def pop_artcl():
-    """Return articles have been accessed the most (Top 3)."""
+# Function to run quertes on the Database, it takes the query as input and returns results
+def db_execute(query):
+    db = psycopg2.connect("dbname=news")
     cur = db.cursor()
-    cur.execute(pop_artcl_qry)
-    return cur.fetchall()
+    cur.execute(query)
+    results = cur.fetchall()
     cur.close()
     db.close()
+    return results
 
 
-def pop_auth():
-    """Return author names got the most page views."""
-    cur = db.cursor()
-    cur.execute(pop_auth_qry)
-    return cur.fetchall()
-    cur.close()
-    db.close()
+# Function to generate the report when the code is executed directly
+def main():
+    """Generate Report."""
+    print('')
+    print('Most Popular Articles')
+    print('---------------------')
+    for x, y in db_execute(pop_artcl_qry):
+        print('"{0}" -- {1} views'
+              .format(x, y))
+    print('')
+    print('Most Popular Authors')
+    print('--------------------')
+    for x, y in db_execute(pop_auth_qry):
+        print('{0} -- {1} views'
+              .format(x, y))
+    print('')
+    print('Days with error rate higher than 1%')
+    print('-----------------------------------')
+    for x, y in db_execute(day_error_qry):
+        print('{0} -- {1:2f}%'
+              .format(x, y))
 
 
-def day_error():
-    """Return days which got more than 1% of requests lead to errors."""
-    cur = db.cursor()
-    cur.execute(day_error_qry)
-    return cur.fetchall()
-    cur.close()
-    db.close()
-
-
-print('')
-print('Most Popular Articles')
-print('---------------------')
-for x, y in pop_artcl():
-    print('"{0}" -- {1} views'
-          .format(x, y))
-print('')
-print('Most Popular Authors')
-print('--------------------')
-for x, y in pop_auth():
-    print('{0} -- {1} views'
-          .format(x, y))
-print('')
-print('Days with error rate higher than 1%')
-print('-----------------------------------')
-for x, y in day_error():
-    # print integer and float value
-    print('{0} -- {1:2f}%'
-          .format(x, y))
+# Condition to check whether the code is executed directly to call the main subroutine
+# This program can be imported as a module to utilize the db_execute function.
+if __name__ == '__main__':
+    main()
--- readme.md
+++ readme.md
@@ -1,24 +1,26 @@
 # Logs Analysis
 
-This is a reporting tool that prints out three reports based on the data in the database. It is a Python program using the psycopg2 module to connect to the "news" database.
-
+This project sets up a mock PostgreSQL database for a fictional news website. The provided Python script uses the psycopg2 library to query the database and produce a report that answers the following three questions:
+* What are the most popular three articles of all time?
+* Who are the most popular article authors of all time?
+* On which days did more than 1% of requests lead to errors?
 ## Getting Started
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. 
 
 ### Prerequisites
 
-You will need a system that has PostgreSQL and Python3 installed to be able to run this code. This project assumes you have these already setup. If you don't, please find instructions on Google based on your system (Windows/MacOS/Unix).
+You will need a system that has PostgreSQL and Python3 installed to be able to run this code. This project assumes you have these already installed, along with a blank "news" database to populate the mock dataset. If you don't, please find instructions on Google based on your system (Windows/MacOS/Unix).
 
 
 ### Installing
 
-You can install psql and python on your system or use a VM that already has these installed.
+You can install psql and python on your system or use a VM that already has these installed. (Recommended to use Vagrant as VM, you can use [this Vagrantfile](https://github.com/udacity/fullstack-nanodegree-vm/blob/master/vagrant/Vagrantfile) provided by Udacity to set up your virtual machine with a blank news database)
 
 Download the project from github and create a folder named "LogAnalysis" and unzip the files.
-Next, download the data [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip). You will need to unzip this file after downloading it. The file inside is called newsdata.sql. Put this file into the "LogAnalysis" folder you just created.
+Next, download the data for news database [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip). You will need to unzip this file after downloading it. The file inside is called newsdata.sql. Put this file into the "LogAnalysis" folder you just created.
 
-To load the data, cd into the vagrant directory and use the command 
+To load the data, cd into the vagrant directory and use the command below (Make sure you have a blank news database if you didn't use the Vagrantfile above)
 ```
 psql -d news -f newsdata.sql
 ```
@@ -27,17 +29,17 @@ Here's what this command does:
 ```
 psql — the PostgreSQL command line program
 
--d news — connect to the database named news which has been set up for you
+-d news — connect to the database named news (created by the vagrantfile, or by you if you are not using a VM)
 
 -f newsdata.sql — run the SQL statements in the file newsdata.sql
 ```
 
 Running this command will connect to your installed database server and execute the SQL commands in the downloaded file, creating tables and populating them with data. After this is completed go into the "LogAnalysis" folder on your console and type the command below to run the report;
 ```
-python log.py
+python3 logs.py
 ```
 
-## Authors
+## References / Authors
 
 * **Kaan Eroglu** - *Initial work* - [kaan.ca](https://www.kaan.ca)
 * **Udacity FSND Instructions** - *Installation instructions*
